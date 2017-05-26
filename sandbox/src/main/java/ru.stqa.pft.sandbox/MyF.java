cfa211ab0ddff782650dc76496949e9bb2339aed
Anastasiya17 <Anastasiyatsybulko@gmail.com>
2017-05-26 21:19:13 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ru.stqa.pft.sandbox/MyF.java
+++ sandbox/src/main/java/ru.stqa.pft.sandbox/MyF.java
@@ -1,7 +1,31 @@
 package ru.stqa.pft.sandbox;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class MyF {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        hello("world");
+
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + "=" + area(len));
+
+        double a = 6;
+        double b = 7;
+        System.out.println("Площадь прямоугольника со сторонами " + a + "и" + b + "=" + area(a,b));
+
+    }
+
+
+    public static void hello(String somebody) {
+        System.out.println("hello " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b){
+
+        return a*b;
     }
 }
