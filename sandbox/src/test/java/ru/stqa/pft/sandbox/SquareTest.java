75b4a2950f03bdf16a1cd335639050e9f05f5b00
Anastasiya17 <Anastasiyatsybulko@gmail.com>
2017-06-30 15:58:56 +0300

    pft-1

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTest.java
@@ -1,8 +1,13 @@
 package ru.stqa.pft.sandbox;
+<<<<<<< HEAD
+=======
+
+>>>>>>> origin/master
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
+<<<<<<< HEAD
  * Created by Анастасия Цыбулько on 21.06.2017.
  */
 public class SquareTest {
@@ -25,3 +30,27 @@ public class SquareTest {
                 }
         }
 
+=======
+ * Created by Dmitriy on 07.06.2017.
+ */
+public class SquareTest {
+
+    @Test
+    public void testPoint() {
+        Point borsh = new Point(0.0, 0.0);
+        Assert.assertEquals(borsh.getDistance(new Point(5.0, 5.0)), 7.0710678118654755);
+    }
+
+    @Test
+    public void testPoint2() {
+        Point borsh = new Point(-0.0, 0.0);
+        Assert.assertEquals(borsh.getDistance(new Point(-5.0, -5.0)), 7.0710678118654755);
+    }
+
+    @Test
+    public void testPoint3() {
+        Point borsh = new Point(500.0, 1532.43);
+        Assert.assertEquals(borsh.getDistance(new Point(50.0, 1.0)), 1596.1760068676638);
+    }
+}
+>>>>>>> origin/master
