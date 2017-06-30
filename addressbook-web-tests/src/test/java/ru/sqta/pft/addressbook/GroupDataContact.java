fb9cd3505ad939159394c21b23e6696b6df2fb4e
Anastasiya17 <Anastasiyatsybulko@gmail.com>
2017-06-30 16:27:43 +0300

    Создание группы контактов.

---
+++ addressbook-web-tests/src/test/java/ru/sqta/pft/addressbook/GroupDataContact.java
@@ -0,0 +1,31 @@
+package ru.sqta.pft.addressbook;
+
+public class GroupDataContact {
+    private final String name;
+    private final String secondName;
+    private final String lastName;
+    private final String address;
+
+    public GroupDataContact(String name, String secondName, String lastName, String address) {
+        this.name = name;
+        this.secondName = secondName;
+        this.lastName = lastName;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
