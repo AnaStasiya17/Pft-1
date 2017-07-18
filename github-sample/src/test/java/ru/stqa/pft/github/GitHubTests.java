2aa389b6dfe5fd9051d636c30f8ef554913950f7
Anastasiya17 <Anastasiyatsybulko@gmail.com>
2017-07-18 22:13:09 +0300

    Лекция 10.1. Сервер непрерывной интеграции

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GitHubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GitHubTests {
+
+  @Test
+  public void testCommits() throws IOException {
+    Github github = new RtGithub("f73c08610d55c374d2b9fd47ea5c94580817cc1e");
+    RepoCommits commits = github.repos().get(new Coordinates.Simple("AnaStasiya17", "Pft-1")).commits();
+    for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+      System.out.println(new RepoCommit.Smart(commit).message());
+    }
+  }
+}
